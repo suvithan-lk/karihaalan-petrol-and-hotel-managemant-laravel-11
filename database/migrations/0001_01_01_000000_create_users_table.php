6bf08a6103d5deea6d0e5c31ad10e7d0f00e761f
A_Suvithan <suvithan.lk@gmail.com>
2026-09-09 14:01:27 +0530

    Add user database constraints

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,19 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            // $table->string('email')->unique();
-            $table->string('phone');
+            $table->string('phone')->unique();
             $table->timestamp('phone_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role',['admin', 'petrol', 'hotel']);
+            $table->enum('role', ['admin', 'petrol', 'hotel'])->default('petrol');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -39,9 +35,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
